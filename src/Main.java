2281fa46ffeeb8f5a9766ac3dbc25320d18ee2f8
george <george.maxi@ucsp.edu.pe>
2025-12-10 22:07:01 -0500

    agregamos mensajes por consola

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hola a todos como estan");
+        System.out.println("Esto no esta en staging ... por el momento");
 
     }
 }
